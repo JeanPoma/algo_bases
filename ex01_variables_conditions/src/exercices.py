604083bf9affb3d5339403f4a250299a013e0c2b
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-09-15 10:37:50 +0200

    test passed : somme

--- ex01_variables_conditions/src/exercices.py
+++ ex01_variables_conditions/src/exercices.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 
 def somme(a: int, b: int) -> int:
     """Retourne la somme de deux entiers."""
-    # TODO
-    raise NotImplementedError
+    return a + b
 
 
 def produit(a: int, b: int) -> int:
@@ -29,14 +28,14 @@ def calcul_reduction(prix: float, taux: float) -> float:
     raise NotImplementedError
 
 
-def est_bissextile(annee: int) -> bool:
+def est_bissextile(year: int) -> bool:
     """Vrai si année bissextile (Grégorien).
     Une année est bissextile si elle est divisible par 4.
     Cependant, elle n'est pas bissextile si elle est divisible par 100, sauf si elle est aussi divisible par 400.
     Par exemple :
         - 2000 est bissextile (divisible par 400).
-        - 1900 n'est pas bissextile (divisible par 100 mais pas par 400).
-        - 2004 est bissextile (divisible par 4 mais pas par 100).
+        - 1900 n'est pas bissextile (divisible par 100, mais pas par 400).
+        - 2004 est bissextile (divisible par 4, mais pas par 100).
     """
     raise NotImplementedError
 
@@ -53,10 +52,10 @@ def maximum_trois(a: int, b: int, c: int) -> int:
 def factorielle(n: int) -> int:
     """Retourne la factorielle d'un entier.
     1. Vérifier si n est un nombre négatif :
-       - Si oui, lever une exception avec un message d'erreur approprié.
+        – Si oui, lever une exception avec un message d'erreur approprié.
     2. Initialiser une variable résultat à 1.
     3. Pour chaque valeur i de 1 à n inclus :
-       - Multiplier le résultat actuel par i.
+        – Multiplier le résultat actuel par i.
     4. Retourner le résultat.
     """
     # TODO
